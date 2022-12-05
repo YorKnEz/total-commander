793e0e8c39ec7e3a1be608d9603680e3567c4adb
Vexionix <djdarkstefan@gmail.com>
2022-12-05 14:35:50 +0200

    remove unused parsing function

--- src/utils.cpp
+++ src/utils.cpp
@@ -35,49 +35,6 @@ string int2str(int a) {
   return stringNumber;
 }
 
-// parses a string into a Filedata structure
-Filedata parseFileDataString(string filedataString) {
-  // remove extra spaces from string
-  for (int i = 1; i < filedataString.size(); i++) {
-    if (filedataString[i] == filedataString[i - 1] &&
-        filedataString[i] == ' ') {
-      filedataString.erase(i--, 1);
-    }
-  }
-
-  // the structure of the file data string is:
-  // DATE TIME AM/PM <DIR>/SIZE NAME
-  Filedata filedata;
-
-  int spacePos = -1;
-
-  for (int i = 0; i < 3; i++) {
-    spacePos = filedataString.find(' ', spacePos + 1);
-  }
-
-  filedata.date = filedataString.substr(0, spacePos);
-
-  int nextSpace = filedataString.find(' ', spacePos + 1);
-
-  filedata.size =
-      filedataString.substr(spacePos + 1, nextSpace - (spacePos + 1));
-
-  spacePos = nextSpace;
-
-  filedata.filename = filedataString.substr(
-      nextSpace + 1, filedataString.size() - (nextSpace + 1));
-
-  int lastDotPos = filedata.filename.find_last_of('.');
-
-  if (lastDotPos != string::npos && filedata.size.compare("<DIR>")) {
-    filedata.ext = filedata.filename.substr(lastDotPos + 1);
-    filedata.filename.erase(filedata.filename.find_last_of('.'));
-  } else
-    filedata.ext = "";
-
-  return filedata;
-}
-
 // this function will convert strings that of format "Sun Nov 17 13:01:15 2022"
 // to "17/11/2022 01:01 PM"
 string formatDate(string date) {
