b41debf8b90a500fb92d36923f075181cf5a3a69
Vexionix <djdarkstefan@gmail.com>
2022-12-15 15:32:22 +0200

    add comments for file related functions in header

--- include/filetree.h
+++ include/filetree.h
@@ -32,12 +32,16 @@ void editFileName(string path, string newName);
 // deletes a file from a specified path
 void deleteFile(string path);
 
-// deletes a folder (requires confirmation in terminal)
+// deletes a folder from a specified path and its components
 void deleteFolder(string path);
+
+// copies a folder and its components from a path to another path
+void copyFolder(string fromPath, string toPath);
 
 // creates a new folder
 void createFolder(string path, string name);
 
+// generates a list of files containing data about the path's content
 void getFilesFromPath(list &l, string path);
 
 void sortFiletree(list &l, sortBy criteria, sortOrder order);
